9e174f2c83cb59eb49af0a3279df0d5fa4831131
Robert Karson <robbykarson@gmail.com>
2014-06-30 22:11:53 -0400

    Attempt to render the header

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "pages#home"
   get "about" => "pages#about"
-  get "team" => "pages#team"
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
