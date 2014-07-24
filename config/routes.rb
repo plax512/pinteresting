0174b133fca594c28bfde96385426e543a70dedb
robert karson <rob.karson@gmail.com>
2014-07-23 20:02:32 -0400

    push to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :pins
+
   devise_for :users
   root "pages#home"
   get "about" => "pages#about"
+  get "pins" => "pages#pins"
   
 
   # The priority is based upon order of creation: first created -> highest priority.
